e31942c6b3f13dca7edc19dd2c4ac3e6ef016676
clear-head <134698515+Clear-head@users.noreply.github.com>
2025-05-25 20:43:07 +0900

    demo: 2025_05_25 사석훈

---
+++ cur_study/Kruskal/BOJ14621/SSH.py
@@ -0,0 +1,47 @@
+import sys
+
+
+def find(p, x):
+    if p[x] != x:
+        p[x] = find(p, parent[x])
+    return p[x]
+
+
+def union(p, x, y):
+    find_a = find(p, x)
+    find_b = find(p, y)
+
+    if find_a < find_b:
+        p[find_b] = find_a
+    else:
+        p[find_a] = find_b
+
+    return p
+
+
+N, M = map(int, sys.stdin.readline().split())
+
+gender = list(map(str, sys.stdin.readline().rstrip().split()))
+
+parent = [i for i in range(N+1)]
+edges = []
+weight = 0
+cnt = 0
+
+for i in range(M):
+    u, v, d = map(int, sys.stdin.readline().split())
+    edges.append((u, v, d))
+
+edges = sorted(edges, key=lambda x: x[2])
+
+for a, b, c in edges:
+    if find(parent, a) != find(parent, b) and gender[a - 1] != gender[b - 1]:
+        parent = union(parent, a, b)
+        weight += c
+        cnt += 1
+
+
+if cnt != N-1:
+    print(-1)
+else:
+    print(weight)
