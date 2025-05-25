5d4301b28ed0c1bb1437b2108069bf726cf4668c
Dev-PKH <tapasse12@gmail.com>
2025-05-25 20:42:05 +0900

    demo: 2025_05_25 박강현
    - 크루스칼 구현
    - 입력에서 처리

---
+++ cur_study/Kruskal/BOJ14621/PKH.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+tuple<int, int, int> g[10005];
+char mw[1005];
+int n,m, ans, cnt;
+
+vector<int> p(1005,-1);
+
+int find(int x)
+{
+	if(p[x] < 0) return x;
+	else return p[x] = find(p[x]);
+}
+
+bool uni(int u, int v)
+{
+	u = find(u); v = find(v);
+	if(u==v) return 0;
+	if(p[u] == p[v]) p[u]--;
+	if(p[u] < p[v]) p[v] = u;
+	else p[u] = v;
+	return 1;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> m;
+	
+	for(int i=1; i<=n; i++) cin >> mw[i];
+	
+	for(int i=0; i<m; i++)
+	{
+		int cost, a, b;
+		cin >> a >> b >> cost;
+		if(mw[a] == mw[b]) continue;
+		g[cnt++] = {cost,a,b};
+	}
+	
+	sort(g, g+cnt);
+	int temp = 0;
+	
+	for(int i=0; i<cnt; i++)
+	{
+		int cost, a, b;
+		tie(cost, a, b) = g[i];
+		if(!uni(a,b)) continue;
+		
+		temp++;
+		ans += cost;
+		if(temp == n-1) break;
+	}
+	
+	if(temp < n-1) cout << -1;
+	else cout << ans;
+	
+	return 0;
+}
