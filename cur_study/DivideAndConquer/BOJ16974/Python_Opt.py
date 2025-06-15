d249ae6e48c333e53df8691e94622eb99dcf8f20
dksldhodkseho <156566854+dksldhodkseho@users.noreply.github.com>
2025-06-15 18:59:44 +0900

    refactor: 2025_06_15 파이썬 최적화

---
+++ cur_study/DivideAndConquer/BOJ16974/Python_Opt.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n, x = map(int, input().split())
+
+layer = [1] * (n+1)
+cnt_p = [1] * (n+1)
+
+for i in range(1, n+1):
+    layer[i] = 2 * layer[i - 1] + 3
+    cnt_p[i] = 2 * cnt_p[i - 1] + 1
+
+def eat(n, x):
+    if n == 0:
+        return 1 if x >= 1 else 0
+    
+    mid = layer[n - 1] + 2
+    
+    if x == 1:
+        return 0
+    
+    elif x <= 1 + layer[n-1]:
+        return eat(n-1, x-1)
+        
+    elif x == mid:
+        return cnt_p[n-1] + 1
+    
+    elif x < layer[n]:
+        return cnt_p[n-1] + 1 + eat(n-1, x - mid)
+        
+    else: 
+        return cnt_p[n]
+
+print(eat(n,x))
