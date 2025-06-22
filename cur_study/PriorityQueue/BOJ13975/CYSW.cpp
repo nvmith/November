0c358bfac46be45f052271df95eec91a4db1f840
nvmith <156288478+nvmith@users.noreply.github.com>
2025-06-22 15:06:25 +0900

    demo: 2025_06_22 최이승우

---
+++ cur_study/PriorityQueue/BOJ13975/CYSW.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   
+   int t, k;
+   
+   cin >> t;
+   
+   while(t--)
+   {
+       ll cost = 0;
+       
+       cin >> k;
+       
+       priority_queue<ll, vector<ll>, greater<ll>> pq;
+       
+       for(int i=0; i<k; i++)
+       {
+          ll a;
+          cin >> a;
+          pq.push(a);
+       }
+       
+       while(pq.size()!=1)
+       {
+          ll a = pq.top(); pq.pop();
+          a += pq.top(); pq.pop();
+          cost += a;
+          pq.push(a);
+       }
+       
+       cout << cost << '\n';
+   }
+   
+   return 0;
+}
