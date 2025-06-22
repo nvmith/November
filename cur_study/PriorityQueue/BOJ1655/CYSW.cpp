3bf339ee7b9d6e8c854937b0ee2ea54f2b20b888
nvmith <156288478+nvmith@users.noreply.github.com>
2025-06-22 17:15:46 +0900

    demo: 2025_06_22 최이승우

---
+++ cur_study/PriorityQueue/BOJ1655/CYSW.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   
+   int n;
+   cin >> n;
+   priority_queue<int, vector<int>, greater<int>> min_pq;
+   priority_queue<int> max_pq;
+   
+   for(int i=0;i<n;i++)
+   {
+       int a;
+       cin >> a;
+       if(max_pq.empty() || a <= max_pq.top()) max_pq.push(a);
+       else min_pq.push(a);
+       if(min_pq.size()+1 < max_pq.size())
+       {
+           min_pq.push(max_pq.top()); max_pq.pop();
+       }
+       else if(min_pq.size() > max_pq.size())
+       {
+           max_pq.push(min_pq.top()); min_pq.pop();
+       }
+       cout << max_pq.top() << '\n';
+   }
+   
+   return 0;
+}
