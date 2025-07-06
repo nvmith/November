9b08f018fe58deea207f3caa4f2d186c99d8ca70
nvmith <156288478+nvmith@users.noreply.github.com>
2025-07-06 19:16:16 +0900

    refactor: 2025_07_06 C++ 최적화 코드
    - 박강현, 최이승우

---
+++ cur_study/BackTracking/BOJ7490/C++_Opt.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+vector<string> result;
+
+void func(int next, string st, int cur, int last)
+{
+    if (next > n)
+    {
+        if (cur == 0)
+            result.push_back(st);
+        return;
+    }
+    
+    func(next + 1, st + "+" + to_string(next), cur + next, next);
+    func(next + 1, st + "-" + to_string(next), cur - next, -next);
+    int space = last >= 0 ? last * 10 + next : last * 10 - next;
+    func(next + 1, st + " " + to_string(next), cur - last + space, space);
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    
+    cin >> t;
+    
+    while(t--)
+    {
+        cin >> n;
+        
+        result.clear();
+        
+        func(2, "1", 1, 1);
+        
+        sort(result.begin(), result.end());
+        
+        for(int i=0;i<result.size();i++)
+            cout << result[i] << '\n';
+        
+        cout << '\n';
+    }
+}
