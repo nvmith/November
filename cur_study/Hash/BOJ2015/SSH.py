43d6c98f28c4939b74465f811f92fc2a08dc2f9f
clear-head <134698515+Clear-head@users.noreply.github.com>
2025-07-20 18:47:09 +0900

    demo: 2025_07_18 사석훈

---
+++ cur_study/Hash/BOJ2015/SSH.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+ans = 0
+n, k = map(int, stdin.readline().rstrip().split())
+a = list(map(int, stdin.readline().rstrip().split()))
+
+nums = {0:1}
+
+prefix_sum = 0
+
+for i in a:
+    prefix_sum += i
+    ans += nums.get(prefix_sum - k, 0)
+    nums[prefix_sum] = nums.get(prefix_sum, 0) + 1
+
+
+print(ans)
