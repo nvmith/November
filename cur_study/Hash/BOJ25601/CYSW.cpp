5db98c779ceb320bebaf27aa59dc3ade11f71b91
nvmith <156288478+nvmith@users.noreply.github.com>
2025-08-03 16:19:50 +0900

    demo: 2025_08_03 최이승우

---
+++ cur_study/Hash/BOJ25601/CYSW.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<string, string> um;
+
+bool isParents(string a, string b) 
+{
+    while (um.count(b)) 
+    {
+        b = um[b];
+        if (b == a) return true;
+    }
+    return false;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    string child, parent;
+    
+    for (int i=0; i<n-1; i++) 
+    {
+        cin >> child >> parent;
+        um[child] = parent;
+    }
+
+    cin >> child >> parent;
+
+    if (isParents(child, parent) || isParents(parent, child))
+        cout << 1;
+    else
+        cout << 0;
+}
