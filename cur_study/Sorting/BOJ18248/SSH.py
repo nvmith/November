adf1c0662907f2ab786b67a313666dc2565aa237
clear-head <134698515+Clear-head@users.noreply.github.com>
2025-09-14 16:43:46 +0900

    demo: BOJ18248 사석훈

--- cur_study/Sorting/BOJ18248/SSH.py
+++ cur_study/Sorting/BOJ18248/SSH.py
@@ -1,18 +1,15 @@
 from sys import stdin, exit
 
 n, m = map(int, stdin.readline().rstrip().split())
-if m == 1:
+if n == 1 or m == 1:
     print("YES")
     exit()
 
 listener = sorted([tuple(map(int, stdin.readline().rstrip().split())) for _ in range(n)])
 
-for i in range(1, m):
-    flag_1 = False  # 이전 것은 못듣고 다음 것은 들을때
-    for people in listener:
-        if people[i-1] == 0 and people[i] == 1:
-            flag_1 = True
-        elif flag_1 and people[i-1] == 1 and people[i] == 0:
+for i in range(0, m-1):
+    for j in range(1, m):
+        if sorted(listener, key=lambda x:x[i]) != sorted(listener, key=lambda x:x[j]):
             print("NO")
             exit()
 
