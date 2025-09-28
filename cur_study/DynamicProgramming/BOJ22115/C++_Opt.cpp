ee28d76f9f680889456394543aa5a932070fb8a0
nvmith <156288478+nvmith@users.noreply.github.com>
2025-09-28 16:46:33 +0900

    refactor: BOJ22115 C++ 최적화 코드
    - 최이승우

---
+++ cur_study/DynamicProgramming/BOJ22115/C++_Opt.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, k;
+    cin >> n >> k;
+    vector<int> c(n);
+    
+    for(int i=0; i<n; i++) 
+        cin >> c[i];
+
+    int over = k+1;
+    vector<int> dp(k+1, over);
+    dp[0] = 0;
+
+    for(int x : c) 
+        for(int s=k; s>=x; s--) 
+            if(dp[s-x] != over)
+                dp[s] = min(dp[s], dp[s-x]+1);
+
+    if(dp[k] == over)
+        cout << -1;
+    else 
+        cout << dp[k];
+}
