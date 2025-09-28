39dca41e6c2644ba63ed076e6333a873e7f5d9c4
Ji Yongju <156566854+dksldhodkseho@users.noreply.github.com>
2025-09-28 18:38:45 +0900

    refactor: BOJ1083 파이썬 최적화 코드

---
+++ cur_study/Sorting/BOJ1083/Python_Opt.py
@@ -0,0 +1,19 @@
+n = int(input())
+num = list(map(int, input().split()))
+s = int(input())
+
+for i in range(n):
+    max_pos = i
+    for j in range(i + 1, min(n, i + s + 1)):
+        if num[j] > num[max_pos]:
+            max_pos = j
+
+    s -= (max_pos - i)
+
+    for j in range(max_pos, i, -1):
+        num[j], num[j - 1] = num[j - 1], num[j]
+
+    if s == 0:
+        break
+
+print(*num)
