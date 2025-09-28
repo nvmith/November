faa470b4e7f7b0203915dd1c4547024a4d5cf9bb
Ji Yongju <156566854+dksldhodkseho@users.noreply.github.com>
2025-09-28 16:42:37 +0900

    refactor: BOJ22115 파이썬 최적화 코드

---
+++ cur_study/DynamicProgramming/BOJ22115/Python_Opt.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+caffein = list(map(int,input().split()))
+
+if k in caffein:
+    print(1)
+elif sum(caffein) < k:
+    print(-1)
+else:    
+    dp = [100001] * (k+1)
+    dp[0] = 0
+    
+    for i in caffein:
+        for j in range(k, i-1, -1):
+            if dp[j-i] != 100001:
+                dp[j] = min(dp[j], dp[j-i]+1)
+    
+    if dp[k] == 100001:
+        print(-1)
+    else:
+        print(dp[k])
